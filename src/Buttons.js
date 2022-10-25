4a751f1cdcfa13d1308a52526e1ec95f86d437b4
armod <armods@gmail.com>
2022-10-25 10:50:47 +0200

    hacker news

---
+++ src/Buttons.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { useGlobalContext } from './context'
+
+const Buttons = () => {
+  return <div>Buttons</div>
+}
+
+export default Buttons
---
+++ src/Stories.js
@@ -0,0 +1,8 @@
+import { useGlobalContext } from './context'
+import React from 'react'
+
+const Stories = () => {
+  return <div>Stories</div>
+}
+
+export default Stories
